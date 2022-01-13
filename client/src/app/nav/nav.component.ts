8f6b5a5254114c86f416e44b25bd2666cfab723d
Muhammad Sohaib <msohaib008@gmail.com>
2022-01-13 18:46:51 +0500

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// import { access } from 'fs';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,21 +14,29 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model:any={}
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router:Router, private toastr: ToastrService) { }
+
+  user:any={};
 
   ngOnInit(): void {
   }
 
   login(){
     this.accountService.login(this.model).subscribe(response =>{
+      this.router.navigateByUrl('/members')
       console.log(response);
+      this.accountService.currentUser$.subscribe(value => this.user=value);
+      console.log("user  ",this.user.username);
+      
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
